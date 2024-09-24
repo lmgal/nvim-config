8a0b0c8f43ba8dee49ca551c413125c778363662
Louie Gallos <lmgallos@up.edu.ph>
2024-09-24 23:32:14 +0800

    Replaced nvim-tree with yazi.nvim

---
+++ README.md
@@ -0,0 +1,5 @@
+# NeoVim Config
+My NeoVim config, just for easier config when installing on some other computers I use
+
+## Requirements
+- [yazi 0.3.0 or later](https://yazi-rs.github.io/docs/installation) 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,13 +1,48 @@
 return {
   {
+    "nvim-tree/nvim-tree.lua",
+    enabled = false,
+  },
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+      -- 👇 in this section, choose your own keymappings!
+      {
+        "<leader>-",
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        -- Open in the current working directory
+        "<leader>cw",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory",
+      },
+      {
+        -- NOTE: this requires a version of yazi that includes
+        -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+        "<c-up>",
+        "<cmd>Yazi toggle<cr>",
+        desc = "Resume the last yazi session",
+      },
+    },
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
+      keymaps = {
+        show_help = "<f1>",
+      },
+    },
+  },
+  {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
   {
     "williamboman/mason.nvim",
-    opts = {
-    }
+    opts = {},
   },
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
@@ -23,20 +58,21 @@ return {
     cmd = "Copilot",
     event = "BufRead",
     config = function()
-      require("copilot").setup({
+      require("copilot").setup {
         suggestion = {
           auto_trigger = true,
           keymap = {
-            accept_word = "<M-w>",
-            accept_line = "<M-$>"
-          }
-        }
-      })
+            accept_suggestion = "<M-l>",
+            accept_word = "<M-j>",
+            accept_line = "<M-k>",
+          },
+        },
+      }
     end,
   },
   {
     "windwp/nvim-ts-autotag",
-    lazy = false
+    event = "BufRead",
   },
   {
     "williamboman/mason-lspconfig.nvim",
@@ -52,16 +88,18 @@ return {
     "ray-x/lsp_signature.nvim",
     event = "VeryLazy",
     opts = {},
-    config = function(_, opts) require'lsp_signature'.setup(opts) end
+    config = function(_, opts)
+      require("lsp_signature").setup(opts)
+    end,
   },
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
-  }
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
 }
