bc056c0cdb3db47d7cc99bbdf9e5da21a8b5a368
Louie Gallos <lmgallos@up.edu.ph>
2024-09-26 15:30:47 +0800

    Upgrade some packages. Added open multiple selected files on telescope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,44 @@
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format('%s %s', 'edit', j.path))
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
+
 return {
   {
     "nvim-tree/nvim-tree.lua",
     enabled = false,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    cmd = "Telescope",
+    opts = function()
+      return require "nvchad.configs.telescope"
+    end,
+    config = function(_, opts)
+      local telescope = require "telescope"
+      opts.defaults.mappings = {
+        i = {
+          ['<CR>'] = select_one_or_multi
+        }
+      }
+      telescope.setup(opts)
+
+      -- load extensions
+      for _, ext in ipairs(opts.extensions_list) do
+        telescope.load_extension(ext)
+      end
+    end,
   },
   {
     "mikavilpas/yazi.nvim",
